e1f8c0f8117091aad69251e29947a994b2fd39e4
manojpopalghat <65394274+manojpopalghat@users.noreply.github.com>
2020-11-06 18:16:16 +0530

    Problem Statement 4 is done

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,36 +1,31 @@
-class StringCalculator
-{
+class StringCalculator {
+	
 	/*
-	 * 3. Allow the Add method to handle new lines between numbers (instead of commas).
-	 */
-    public int add(String numbers)
-    {
-    	int sum = 0;
-    	if(numbers.strip().length()==0)
-    		return 0;
-    	else 
-    	{
-    		char c;
-    		for(int i=0;i<numbers.length();i++)
-    		{
-    			try 
-        		{
-    				c = numbers.charAt(i);
-        			if(c != '\n' && c != ',')
-        				sum += Integer.parseInt(numbers.charAt(i)+"");
-        		}
-        		catch (Exception e) {
-    				System.out.println(e);
-    			}
-    		}
-        	return sum;
-    	}
-    }
-    
-    
-    public static void main(String args[])
-    {
-    	StringCalculator sc = new StringCalculator();
-    	System.out.println(sc.add("1\n2,3"));
-    }
+	 * 4. Support different delimiters
+	 * for example “//;\n1;2” == 3
+	 */ 
+	public int add(String numbers) {
+		int sum = 0;
+		char delimiter = numbers.charAt(2);
+		if (numbers.strip().length() < 4)
+			return 0;
+		else {
+			char c;
+			for (int i = 4; i < numbers.length(); i++) {
+				try {
+					c = numbers.charAt(i);
+					if (c != delimiter)
+						sum += Integer.parseInt(numbers.charAt(i) + "");
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+			return sum;
+		}
+	}
+
+	public static void main(String args[]) {
+		StringCalculator sc = new StringCalculator();
+		System.out.println(sc.add("//;\n1;2;3;4"));
+	}
 }
