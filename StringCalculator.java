4702fc7922d923e2f03d1f92a542ea9ef1b6b869
“manojpopalghat” <manojpopalghat@gmail.com>
2020-11-06 17:10:22 +0530

    Problem Statement 1 is Done

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,8 +1,25 @@
 class StringCalculator
 {
+	/*
+	 * 1) returns upto two numbers
+	 */
     public int Add(String numbers)
     {
     	
-        return "";
+    	if(numbers.length() == 0)
+    		return 0;
+    	else if(numbers.length() == 1)
+    		return Integer.parseInt(numbers);
+    	else 
+    	{
+    		int sum = Integer.parseInt(numbers.charAt(0)+"")+Integer.parseInt(numbers.charAt(2)+"");
+    		return sum;
+    	}    	
+    }
+    
+    public static void main(String args[])
+    {
+    	StringCalculator sc = new StringCalculator();
+    	System.out.println(sc.Add("1,2"));
     }
 }
