b4740519b9c1e066d6a4f9327057e89f8328e85b
manojpopalghat <65394274+manojpopalghat@users.noreply.github.com>
2020-11-06 20:06:45 +0530

    Problem statement 11 is done

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,8 +1,10 @@
+import java.util.Iterator;
+
 class StringCalculator 
 {	
 	/*
-	 * 10.Delimiters can be of any length with the following format: “//[delimiter]\n”
-	 * for example: “//[***]\n1***2***3” == 6
+	 * 11. Allow multiple delimiters like this: “//[delim1][delim2]\n” for example
+	 * “//[*][%]\n1*2%3” == 6.
 	 */	
 	public static int count = 0;
 	
@@ -16,11 +18,17 @@ class StringCalculator
 		count++;
 		int sum = 0;
 		int index1 = numbers.indexOf('[');
-		int index2 = numbers.indexOf(']');
-		String delimiter = numbers.substring(index1+1, index2);
-		int num;
-		String newNumbers = numbers.substring(index2+2);// after ]\n 
-		String str[] = newNumbers.split(delimiter);
+		int index2 = numbers.lastIndexOf(']');
+		String newStr = numbers.substring(index1+1, index2); //getting string between first [ and last ] we get *][%][$
+		
+		String delimArr[] = newStr.split("]["); //removing ][ we get *,%,$
+		int beginIndex = numbers.indexOf('\n')+1;
+		numbers = numbers.substring(beginIndex);
+		for(String delim : delimArr)
+			numbers = numbers.replaceAll(delim, ","); //making all different delimiters to one delimeter ','
+		
+		String str[] = numbers.split(",");
+		int num=0;
 		for (String s:str) 
 		{
 			try 
@@ -44,6 +52,6 @@ class StringCalculator
 	public static void main(String args[])
 	{
 		StringCalculator sc = new StringCalculator();
-		System.out.println(sc.add("//[;;;]\n1;;;2;;;3"));
+		System.out.println(sc.add("//[$][#]\n1$2#3"));
 	}
 }
